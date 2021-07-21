32d19a98edcc386bdecb874d4c90c334d2806e3d
Preethi <writetopreethir@gmail.com>
2021-07-21 17:11:58 +0400

    First Route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 
-
+//First Route
+app.get('/', (req, res) => (res.send('Hello World! My First Route...')));
 const port = 5500;
 app.listen(port, () => console.log(`Server running on Port ${port}!`));
