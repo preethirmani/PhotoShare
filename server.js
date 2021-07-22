0da00d66b3a64edd01f8d944375f662fe6a2657a
Preethi <writetopreethir@gmail.com>
2021-07-22 18:14:55 +0400

    db connected

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 const app = express();
+
+
+//db connect
+const db = keys.mongoURI;
+mongoose.connect(db)
+        .then(() => console.log("Mongo DB connected!"))
+        .catch(err => console.log(err))
 
 
 //First Route
