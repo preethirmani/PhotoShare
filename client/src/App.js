d69c2b9a54eb9ffab32e80e45839fcfc2315fc15
Preethi <writetopreethir@gmail.com>
2021-08-23 15:54:22 +0400

    Logout implemented

--- client/src/App.js
+++ client/src/App.js
@@ -17,28 +17,24 @@ import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 import { SET_CURRENT_USER } from './actions/types';
+import { logoutUser } from './actions/authActions';
 
 
   if(localStorage.jwtToken){
     //set token to auth header
     setAuthToken(localStorage.jwtToken);
-
     //Decode Token
     const decoded = jwt_decode(localStorage.jwtToken);
 
-    //write to the store
-    store.dispatch({
-      type: SET_CURRENT_USER,
-      payload:decoded
-    })
-
-    window.location.href = '/home';
-
+    const currentTime = Date.now()/1000;
+    if (decoded.exp < currentTime){
+     
+      store.dispatch(logoutUser());
+      window.location.href = "/login";
+    }
   }
 
 class App extends Component {
-
-
 
   render() {
     return (
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,44 +1,71 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logoutUser } from '../../actions/authActions';
 import home from '../../img/home.png';
 import newPost from '../../img/newPost.png';
 import allPosts from '../../img/allPosts.png';
 import like from '../../img/like.png';
 import '../../css/navBar.css'
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+
+  onLogoutClick(e) {
+    console.log('logout clicked')
+    e.preventDefault();
+    this.props.logoutUser();
+    this.props.history.push('/');
+  }
   render() {
+   const { isauthenticated, user }=this.props.auth;
+
     return (
       
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-        <h3 class=" brand-name" to="#">PhotoShare</h3>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+    <nav className= "navbar navbar-expand-lg navbar-light">
+      <div className= "container">
+        <h3 className= " brand-name" to="#">PhotoShare</h3>
+        <button className= "navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
           aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span className= "navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="#">
-                <img class="img-navbar" src={home} /></Link>
+        <div className= "collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
+          <ul className= "navbar-nav">
+            <li className= "nav-item">
+              <Link className= "nav-link active" aria-current="page" to="/home">
+                <img className= "img-navbar" src={home} /></Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="#">
-                <img class="img-navbar" src={newPost}/>
+            <li className= "nav-item">
+              <Link className= "nav-link" to="#">
+                <img className= "img-navbar" src={newPost}/>
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="#">
-                <img class="img-navbar" src={allPosts}/>
+            <li className= "nav-item">
+              <Link className="nav-link" to="#">
+                <img className="img-navbar" src={allPosts}/>
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="#">
-                <img class="img-navbar" src={like}/>
+            <li className= "nav-item">
+              <Link className= "nav-link" to="#">
+                <img className= "img-navbar" src={like}/>
               </Link>
             </li>
-
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown" role="button" 
+             aria-expanded="false"
+               href="#" >
+               <img className="rounded-circle"
+               src={user.avatar} alt={user.name} style={{ width: "25px", marginRight: "5px" }}/>
+              
+               </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className ="dropdown-item" href="/profile">Profile</a></li>
+                  <li><a className ="dropdown-item" onClick={this.onLogoutClick.bind(this)} > Logout
+                  </a>
+                  </li>
+                </ul>
+            </li>
           </ul>
         </div>
       </div>
@@ -46,3 +73,15 @@ export default class Navbar extends Component {
     )
   }
 }
+
+Navbar.propTypes = {
+  logoutUser : PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+ 
+})
+
+export default connect(mapStateToProps, {logoutUser}) (Navbar);
